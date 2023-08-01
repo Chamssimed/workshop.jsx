97b32597d2b1100c309629e525253840e72c7883
Chamssimed <133645684+Chamssimed@users.noreply.github.com>
2023-08-01 10:14:03 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FullName from './components/FullName';
+
+function App() {
+  return (
+    <div className="App">
+     <FullName />
+     
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/FullName.js
@@ -0,0 +1,20 @@
+const FullName = () => {
+  return (
+    <div style={{ margin: "50px" }}>
+      <h1>mercedes G-class</h1>
+
+      <img
+        src="https://f7432d8eadcf865aa9d9-9c672a3a4ecaaacdf2fee3b3e6fd2716.ssl.cf3.rackcdn.com/C2299/U979/IMG_77247-large.jpg"
+        alt="img"
+      />
+      <p>
+        Il est offert en version 550 et AMG 63. La puissance développée par le
+        moteur V8 de 4 litres est respectivement de 416 et 577 chevaux. Il est
+        assemblé en Autriche.
+      </p>
+      <h5>prix:208 950 €</h5>
+    </div>
+  );
+};
+
+export default FullName
